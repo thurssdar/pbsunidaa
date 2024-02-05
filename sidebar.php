5b9e9686c309fa859c5ee9bebe32881ddec0d9a8
thurssdar <145630790+thurssdar@users.noreply.github.com>
2024-02-05 16:35:30 +0700

    Sidebar dan Home

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@ $sidebar ='#637A9F';
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: <?php echo $sidebar; ?> ;">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light" style="color:#ffffff ;">PBS UNIDA</span>
       <link rel="stylesheet" href="css/bootstrap.css">
